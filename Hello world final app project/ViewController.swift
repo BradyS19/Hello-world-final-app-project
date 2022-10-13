78f7e361c772af00e17183abf90a9ffbd441b26b
brady scruton <bscru062@d127.org>
2022-10-13 09:13:06 -0500

    Initial Commit

---
+++ Hello world final app project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello world final app project
+//
+//  Created by Brady C Scruton (Student) on 10/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hello world final app projectUITests/Hello_world_final_app_projectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hello_world_final_app_projectUITestsLaunchTests.swift
+//  Hello world final app projectUITests
+//
+//  Created by Brady C Scruton (Student) on 10/13/22.
+//
+
+import XCTest
+
+class Hello_world_final_app_projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
